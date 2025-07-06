b47456dc5ae39610544d7e392268e829eda479aa
JoaoPopinhak <joaomrpopinhak@hotmail.com>
2025-07-06 15:46:51 -0300

    Update ContaBancariaVoid.java

--- FINANCIS/ContaBancariaVoid.java
+++ FINANCIS/ContaBancariaVoid.java
@@ -11,16 +11,20 @@ public class ContaBancariaVoid {
 		//Validação para cotas repetidas
 		//Validacao de transferencia, deposito e saque
 		
-		while (true) {
+		
+		System.out.println("Clica em uma das opções informadas abaixo:\n\n"
+				+ "Digite 1 para criar uma nova conta bancaria\n"
+				+ "Digite 2 para depositar um valor na conta\n"
+				+ "Digite 3 para sacar um valor da conta\n"
+				+ "Digite 4 para transferir sua conta\n"
+				+ "Digite 5 para exibir suas contas cadastradas e o saldo de cada conta\n"
+				+ "Digite 6 para editar o nome das suas contas\n"
+				+ "Digite 10 para SAIR\n");
+		
+		int opcao = scanner.nextInt();
+		
+		while (opcao != 10) {
 			
-			System.out.println("Clica em uma das opções informadas abaixo:\n\n"
-					+ "Digite 1 para criar uma nova conta bancaria\n"
-					+ "Digite 2 para depositar um valor na conta\n"
-					+ "Digite 3 para sacar um valor da conta\n"
-					+ "Digite 4 para transferir sua conta\n"
-					+ "Digite 5 para exibir suas contas cadastradas e o saldo de cada conta\n");
-			
-			int opcao = scanner.nextInt();
 			
 			if (opcao == 1) {
 			
@@ -103,13 +107,28 @@ public class ContaBancariaVoid {
 			}else if(opcao == 6) {
 				System.out.println("Escolha a conta que deseja editar");
 				for(int i = 0; i < contasBancarias.size(); i++) {
-					System.out.println(i + "CONTA: " + contasBancarias.get(i).nomeconta + "SALDO: " + contasBancarias.get(i).saldo);
+					System.out.println(i + " CONTA: " + contasBancarias.get(i).nomeconta + " SALDO: " + contasBancarias.get(i).saldo);
 				}
 				
+				
 				ContaBancariaClass contabancaria = contasBancarias.get(scanner.nextInt());
+				System.out.println("Digite o novo nome da sua conta");
+				contabancaria.nomeconta = scanner.next();
 				
 			}
 			
-		}	
+			System.out.println("Clica em uma das opções informadas abaixo:\n\n"
+					+ "Digite 1 para criar uma nova conta bancaria\n"
+					+ "Digite 2 para depositar um valor na conta\n"
+					+ "Digite 3 para sacar um valor da conta\n"
+					+ "Digite 4 para transferir sua conta\n"
+					+ "Digite 5 para exibir suas contas cadastradas e o saldo de cada conta\n"
+					+ "Digite 6 para editar o nome das suas contas\n"
+					+ "Digite 10 para SAIR\n");
+			
+			opcao = scanner.nextInt();
+			
+		} //Fim while
+		scanner.close();
 	} 
 }
